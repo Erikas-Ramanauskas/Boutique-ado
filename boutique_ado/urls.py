ec567a74c95856fa7ff413bcb20419a2cfa33872
Chaosas100 <101866007+Chaosas100@users.noreply.github.com>
2023-09-18 07:04:00 +0100

    Initial commit

--- boutique_ado/urls.py
+++ boutique_ado/urls.py
@@ -1,4 +1,4 @@
-"""butique_ado URL Configuration
+"""boutique_ado URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
