6d175307b22b028004110845dbef6a735851a9eb
Chaosas100 <101866007+Chaosas100@users.noreply.github.com>
2023-10-04 23:06:21 +0100

    Final deployment

--- products/models.py
+++ products/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Category(models.Model):
     class Meta:
-        verbose_name_plural = 'Categories'
+        verbose_name_plural = 'Categories'  # Admin panel name
 
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
