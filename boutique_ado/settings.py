0119a549e8f70eab3e4d1a6a420b018147330acc
Chaosas100 <101866007+Chaosas100@users.noreply.github.com>
2023-10-05 23:42:47 +0100

    deployed to heroku 2

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'p@ei#69*b*zz3u4yie-$()@cy^l(+x9&@6ypx+r0lm(3%_9hr7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['botique-ado-erik.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['botique-ado-erik-a1b0e1bf340f.herokuapp.com', 'localhost']
 
 
 # Application definition
